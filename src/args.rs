35e57d88573e580b2c6abfca6e4d7b6219b07686
Wagner Rosa <60220411+wagner-deoliveira@users.noreply.github.com>
2023-10-19 14:01:43 +0200

    More about refactoring

--- src/args.rs
+++ src/args.rs
@@ -2,7 +2,37 @@ use clap::{Args, Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
-pub struct VersifyArgs{
+pub struct VersifyArgs {
+    #[clap(subcommand, name = "github authentication")]
+    pub command: Option<EntityType>,
+}
+
+//TODO: Add support to choose the output directory and subcommand to create a pull request using GitHub API
+
+#[derive(Debug, Subcommand)]
+pub enum EntityType {
+    /// Create a pull request
+    Create(PrCommand),
+    /// Download the packages.txt file from the repository
+    Download(DownloadCommand),
+    /// Update the packages.txt file from the repository
+    Update(UpdateCommand),
+}
+
+
+#[derive(Debug, Args)]
+pub struct PrCommand {
+    pub string: Option<String>,
+}
+
+#[derive(Debug, Args)]
+pub struct DownloadCommand {
+    pub string: Option<String>,
+}
+
+
+#[derive(Debug, Args)]
+pub struct UpdateCommand {
     /// Path to the file directory
     #[arg(short, long)]
     pub path: String,
@@ -10,65 +40,10 @@ pub struct VersifyArgs{
     /// SATK, Mashup, SSC, SSIV, SCE, HCS, ImageImport, ImageDiscovery, SciStream, Metastore.
     #[arg(short, long)]
     pub domain: String,
-    /// The build number of the apps, e.g. 4.0.8.10268
+    /// The build number of the apps, e.g. 3.0.8.10268
     #[arg(short, long)]
     pub build_number: String,
     /// Choose the output directory. By default, it'll create a new directory called "output" in the current working directory
     #[arg(short, long, value_name = "OUTPUT_PATH")]
     pub output: Option<String>,
-    //// Command to create a new pull request
-    // #[clap(subcommand, name = "GITHUB_ACTIONS")]
-    // pub github_actions: Option<EntityType>,
-}
-
-//TODO: Add support to choose the output directory and subcommand to create a pull request using GitHub API
-
-#[derive(Debug, Parser)]
-#[clap(author, version, about)]
-pub struct GitHubActions {
-    #[clap(subcommand, name = "create_pull_request")]
-    pub entity_type: EntityType,
-}
-#[derive(Debug, Subcommand)]
-pub enum EntityType {
-    /// Create a pull request
-    Create(PrCommand),
-    /// Download the packages.txt file from the repository
-    Download(DownloadCommand)
-}
-
-#[derive(Debug, Args)]
-pub struct PrCommand {
-    #[clap(subcommand, name = "create pull request")]
-    pub command: PrSubCommand,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum PrSubCommand {
-    /// Create a pull request
-    Create(PullRequestCommand),
-}
-
-#[derive(Debug, Args)]
-pub struct PullRequestCommand {
-    /// Create a pull request with a specific title
-    pub title: String,
-}
-
-#[derive(Debug, Args)]
-pub struct DownloadCommand {
-    #[clap(subcommand, name = "download file")]
-    pub command: DownloadSubCommand,
-}
-
-#[derive(Debug, Subcommand)]
-pub enum DownloadSubCommand {
-    /// Create a pull request
-    Download(DownloadRequestCommand),
-}
-
-#[derive(Debug, Args)]
-pub struct DownloadRequestCommand {
-    /// Create a pull request with a specific title
-    pub name: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -5,49 +5,49 @@ mod replace_version;
 
 use std::collections::HashMap;
 use std::error::Error;
-use clap::{Command, Parser};
+use clap::Parser;
 use args::VersifyArgs;
 use read_file::read_file;
 use replace_version::replace_version;
-use crate::args::{EntityType, GitHubActions};
+use crate::args::EntityType;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = VersifyArgs::parse();
-    let path = args.path;
-    let domains = args.domain;
-    let versions = args.build_number;
-    let mut output_path= "output";
 
-    if let Some(output) = args.output.as_deref() {
-        output_path = output;
+    match &args.command {
+        None => {
+            Err(Box::try_from("Domain_list and version_list have different sizes. \
+                        Make sure to enter the same number of domains and versions").unwrap(),
+            )
+        }
+        Some(EntityType::Create(name)) => {
+            todo!("To be implemented")
+        }
+        Some(EntityType::Download(name)) => {
+            todo!("To be implemented")
+        }
+        Some(EntityType::Update(name)) => {
+            let path = &name.path;
+            let domains = &name.domain;
+            let versions = &name.build_number;
+            let mut output_path = "output";
+
+            if let Some(output) = &name.output.as_deref() {
+                output_path = output;
+            }
+            let domain_list: Vec<&str> = domains.split(",").collect();
+            let version_list: Vec<&str> = versions.split(",").collect();
+
+            if domain_list.len() != version_list.len() {
+                Err::<EntityType, Box<dyn Error>>(Box::try_from("Domain_list and version_list have different sizes. \
+                        Make sure to enter the same number of domains and versions").unwrap(),
+                ).unwrap();
+            };
+
+            let version_mapping: HashMap<&str, &str> = domain_list.clone().into_iter().zip(version_list.clone().into_iter()).collect();
+
+            let file = read_file(&*path);
+            replace_version(&file, version_mapping, output_path)
+        }
     }
-
-    // if let Some(create_pr) = args.github_actions.as_deref() {
-    //     println!("The text {create_pr}");
-    // }
-
-   // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    // match EntityType {
-    //     Some(EntityType::Create(_) ) => {
-    //         println!("Printing testing lists...");
-    //     }
-    //     None => {}
-    //     _ => {}
-    // }
-
-    let domain_list: Vec<&str> = domains.split(",").collect();
-    let version_list: Vec<&str> = versions.split(",").collect();
-
-    if domain_list.len() != version_list.len() {
-        return Err(
-            Box::try_from("Domain_list and version_list have different sizes. \
-            Make sure to enter the same number of domains and versions").unwrap(),
-        );
-    };
-
-    let version_mapping: HashMap<&str, &str> = domain_list.clone().into_iter().zip(version_list.clone().into_iter()).collect();
-
-    let file = read_file(&*path);
-    replace_version(&file, version_mapping, output_path)
 }
